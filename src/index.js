bd701110e6655b75f88ced25656e4e704b0507eb
SkaymanT <SkaymanT@yandex.ru>
2020-03-04 17:55:18 +0300

    feat: start work brackets

--- src/index.js
+++ src/index.js
@@ -6,55 +6,47 @@ function eval() {
 function expressionCalculator(expr) {
     let result = 0;
     const RegexAll = /[+*/\-]/g;
-    const RegexFirst = /[\\*\\/]/;
-    const RegexSecond = /[+-]/;
     const RegexBracket = /[()]/;
     expr = expr.replace(/\s/g, '');
     let operator = expr.match(RegexAll);
     let buf = expr.split(RegexAll);
-    //let buf1 = expr.split(RegexSecond);
-    //let buf2 = expr.split(RegexBracket);
+    let bracket = expr.match(RegexBracket);
+    if (!(bracket == null)) {
+        if ((bracket.length % 2) == 1) {
+            throw "ExpressionError: Brackets must be paired";
+        }
+    }
     for (let i = 0; i < operator.length; i) {
         if (operator[i] == '/') {
-            if (buf[i+1] == 0) {
+            if (buf[i + 1] == 0) {
                 throw "TypeError: Division by zero.";
             }
             buf[i] = +buf[i] / (+buf[i + 1]);
             buf.splice(i + 1, 1);
             operator.splice(i, 1);
-        }
-        else if (operator[i] == '*') {
+        } else if (operator[i] == '*') {
             buf[i] = +buf[i] * (+buf[i + 1]);
             buf.splice(i + 1, 1);
             operator.splice(i, 1);
-        }
-        else{
+        } else {
             ++i;
         }
     }
 
     for (let i = 0; i < operator.length; i) {
         if (operator[i] == '-') {
-            
+
             buf[i] = +buf[i] - (+buf[i + 1]);
             buf.splice(i + 1, 1);
             operator.splice(i, 1);
-        }
-        else if (operator[i] == '+') {
+        } else if (operator[i] == '+') {
             buf[i] = +buf[i] + (+buf[i + 1]);
             buf.splice(i + 1, 1);
             operator.splice(i, 1);
-        }
-        else{
+        } else {
             ++i;
         }
     }
-
-
-
-    // if(expr=="2+2"){
-    //     return 4;
-    // }
 
     return buf[0];
 }
